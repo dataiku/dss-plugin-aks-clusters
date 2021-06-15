25a8527d8db2515a1433703ec00590af38735477
Jean-Bernard Jansen <jean-bernard.jansen@dataiku.com>
2021-06-15 17:17:28 +0200

    auth: Fix auth to support the new modes

--- python-lib/dku_azure/auth.py
+++ python-lib/dku_azure/auth.py
@@ -3,15 +3,28 @@ from azure.identity import DefaultAzureCredential, ManagedIdentityCredential
 
 from dku_utils.access import _is_none_or_blank
 
+
 def get_credentials_from_connection_info(connection_info, connection_info_secret):
-    user_managed_identity = connection_info.get('userManagedIdentity', None)
-    client_id = connection_info.get('clientId', None)
-    password = connection_info.get('password', None)
-    tenant_id = connection_info.get('tenantId', None)
-    if not _is_none_or_blank(user_managed_identity) and not _is_none_or_blank(client_id) and not _is_none_or_blank(password):
-        credentials = MSIAuthentication(msi_res_id = user_managed_identity)
-    elif not _is_none_or_blank(client_id) and not _is_none_or_blank(tenant_id) and not _is_none_or_blank(password):
+    infos = {}
+    infos.update(connection_info_secret)
+    infos.update(connection_info)
+
+    user_managed_identity = infos.get('userManagedIdentity', None)
+    identity_type = infos.get('identityType','default')
+    if identity_type == 'default':
+        credentials = DefaultAzureCredential()
+    elif identity_type == 'user-assigned-client-id':
+        credentials = ManagedIdentityCredential(client_id=infos.get('userManagedIdentityClientId',None))
+    elif identity_type == 'user-assigned-resource-id':
+        credentials = ManagedIdentityCredential(identity_config={'id': infos.get('userManagedIdentityResourceId',None)})
+    elif identity_type == 'service-principal':
+        client_id = infos.get('clientId', None)
+        password = infos.get('password', None)
+        tenant_id = infos.get('tenantId', None)
         credentials = ServicePrincipalCredentials(client_id = client_id, secret = password, tenant = tenant_id)
     else:
-        return DefaultAzureCredential()
+        raise "Identity type {} is unknown and cannot be used".format(identity_type)
+
     return credentials
+
+
