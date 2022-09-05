0025f5288445bc0d10ef5f7a7accee7d57948b5d
Amandine Souilleux <amandine.souilleux@dataiku.com>
2022-09-05 16:23:51 +0200

    add support for GPUs out-of-the-box

---
+++ python-lib/dku_kube/nvidia_utils.py
@@ -0,0 +1,20 @@
+import os, json, logging
+
+from .kubectl_command import run_with_timeout
+
+def has_gpu_driver(kube_config_path):
+    env = os.environ.copy()
+    env['KUBECONFIG'] = kube_config_path
+    cmd = ['kubectl', 'get', 'pods', '--namespace', 'kube-system', '-l', 'name=nvidia-device-plugin-ds', '--ignore-not-found']
+    logging.info("Checking if NVIDIA GPU drivers are installed with : %s" % json.dumps(cmd))
+    out, err = run_with_timeout(cmd, env=env, timeout=5)
+    return len(out.strip()) > 0
+
+def add_gpu_driver_if_needed(kube_config_path):
+    if not has_gpu_driver(kube_config_path):
+        cmd = ['kubectl', 'apply', '-f', "https://raw.githubusercontent.com/NVIDIA/k8s-device-plugin/master/nvidia-device-plugin.yml"]
+
+        logging.info("Install NVIDIA GPU drivers with : %s" % json.dumps(cmd))
+        env = os.environ.copy()
+        env['KUBECONFIG'] = kube_config_path
+        run_with_timeout(cmd, env=env, timeout=5)
