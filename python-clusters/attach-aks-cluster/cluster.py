b53ff68c5107913ac586825e4f2011a9f775b949
Amandine Souilleux <amandine.souilleux@dataiku.com>
2022-12-09 12:17:44 +0100

    [sc-110915] attach cluster

--- python-clusters/attach-aks-cluster/cluster.py
+++ python-clusters/attach-aks-cluster/cluster.py
@@ -6,7 +6,7 @@ from dku_utils.access import _is_none_or_blank
 from dku_utils.cluster import make_overrides, get_subscription_id
 from dku_azure.auth import get_credentials_from_connection_info, get_credentials_from_connection_infoV2
 from dku_azure.utils import run_and_process_cloud_error
-from dku_azure.utils import run_and_process_cloud_error, get_instance_metadata, get_subscription_id
+from dku_azure.utils import run_and_process_cloud_error, get_instance_metadata, get_subscription_id, patch_kube_config_with_aad
 
 class MyCluster(Cluster):
     def __init__(self, cluster_id, cluster_name, config, plugin_config):
@@ -30,6 +30,13 @@ class MyCluster(Cluster):
         
     def start(self):
         credentials, subscription_id = self._get_credentials()
+        connection_info = self.config.get("connectionInfoV2",{"identityType":"default"})
+        identity_type = connection_info.get("identityType", "default")
+        identity_label = None
+        if identity_type == 'user-assigned':
+            identity_label = connection_info.get("userManagedIdentityId", "")
+        elif identity_type == 'service-principal':
+            identity_label = connection_info.get("clientId", "")
 
         # Cluster name
         cluster_name = self.config.get("cluster", None)
@@ -50,18 +57,24 @@ class MyCluster(Cluster):
         def do_get():
             return clusters_client.managed_clusters.get(resource_group, cluster_name)
         get_cluster_result = run_and_process_cloud_error(do_get)
-        patch_user_in_kubeconfig = get_cluster_result.aad_profile.managed and get_cluster_result.aad_profile.enable_azure_rbac
+        print("Amandine - managed: {}".format(get_cluster_result.aad_profile.managed))
+        patch_user_in_kubeconfig = get_cluster_result.aad_profile.managed
 
         # Get kubeconfig 
         logging.info("Fetching kubeconfig for cluster %s in %s", cluster_name, resource_group)
         def do_fetch():
             return clusters_client.managed_clusters.list_cluster_admin_credentials(resource_group, cluster_name)
         get_credentials_result = run_and_process_cloud_error(do_fetch)
+
         kube_config_content = get_credentials_result.kubeconfigs[0].value.decode('utf8')
         kube_config_path = os.path.join(os.getcwd(), 'kube_config')
+        if patch_user_in_kubeconfig:
+            kube_config_yaml = patch_kube_config_with_aad(kube_config_content, identity_type, identity_label)
+        else:
+            kube_config_yaml = yaml.safe_load(kube_config_content)
         with open(kube_config_path, 'w') as f:
-            f.write(kube_config_content)
-        overrides = make_overrides(self.config, yaml.safe_load(kube_config_content), kube_config_path)
+            yaml.dump(kube_config_yaml, f)
+        overrides = make_overrides(self.config, kube_config_yaml, kube_config_path)
         
         # Get other cluster infos
         def do_inspect():
