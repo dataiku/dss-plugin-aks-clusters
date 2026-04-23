8c015045447f58622b157f8935a0bc82dae6ccd4
vrutz <val.rutz@dataiku.com>
2026-04-23 10:13:30 +0200

    Rename aks access mode to cluster admin for clarity

--- python-lib/dku_utils/aks_access.py
+++ python-lib/dku_utils/aks_access.py
@@ -1,10 +1,10 @@
-AKS_ACCESS_MODE_ADMIN = "admin"
+AKS_ACCESS_MODE_CLUSTER_ADMIN = "cluster-admin"
 AKS_ACCESS_MODE_CLUSTER_USER = "cluster-user"
 
-DEFAULT_AKS_ACCESS_MODE = AKS_ACCESS_MODE_ADMIN
+DEFAULT_AKS_ACCESS_MODE = AKS_ACCESS_MODE_CLUSTER_ADMIN
 AKS_ACCESS_MODE_CHOICES = [
-    {"value": AKS_ACCESS_MODE_ADMIN, "label": "Admin"},
-    {"value": AKS_ACCESS_MODE_CLUSTER_USER, "label": "Cluster user"},
+    {"value": AKS_ACCESS_MODE_CLUSTER_ADMIN, "label": "Cluster Admin"},
+    {"value": AKS_ACCESS_MODE_CLUSTER_USER, "label": "Cluster User"},
 ]
 AKS_ACCESS_MODES = {
     choice["value"] for choice in AKS_ACCESS_MODE_CHOICES
