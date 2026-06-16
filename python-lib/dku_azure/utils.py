d3b1128aa1d04b09729657cccf1a2c7abdedf482
Amandine Souilleux <amandineslx@users.noreply.github.com>
2026-06-16 15:56:18 +0200

    Update python-lib/dku_azure/utils.py
    Co-authored-by: Pierre Jestin <pierre.jestin@dataiku.com>

--- python-lib/dku_azure/utils.py
+++ python-lib/dku_azure/utils.py
@@ -104,7 +104,4 @@ def determine_node_pool_mode(input_node_pool_mode, is_existing_system_node_pool)
         return "System"
 
 def is_explicit_system_node_pool(node_pool_modes):
-    for node_pool_mode in node_pool_modes:
-        if node_pool_mode == "System":
-            return True
-    return False
+    return "System" in node_pool_modes
